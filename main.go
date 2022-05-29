5abac47cf52a80b7e9b2ddf5cf00565fd15cb932
Riku-programming <riku_programming@gmail.com>
2022-05-29 19:21:43 +0900

    fix 処理を変えてwriteLineの関数を削除

--- main.go
+++ main.go
@@ -12,16 +12,16 @@ import (
 	"time"
 )
 
+const HostFileName string = "client_host.txt"
 const latestFileName string = "-latest.txt"
 const compareFileName string = "-compare.txt"
 const versionHistoryFileName string = "-version_history.txt"
 const dateLayout string = "2006-01-02"
 
-//const URL string = "https://test-nesic-cp.axlbox.biz/common/versions.txt"
-
 func main() {
 	now := time.Now()
-	file, err := os.Open("client_host.txt")
+	fmt.Printf("Start: %vms\n", time.Since(now).Milliseconds())
+	file, err := os.Open(HostFileName)
 	if err != nil {
 		log.Fatalf("Error when opening file: %s", err)
 	}
@@ -37,25 +37,22 @@ func main() {
 		if fileExists(clientName+versionHistoryFileName) == false {
 			createFile(clientName+versionHistoryFileName, fetchVersion(URL))
 		}
-		if err := writeLine(clientName+compareFileName, fetchVersion(URL)); err != nil {
-			fmt.Println(os.Stderr, err)
-			os.Exit(1)
-		}
+		createFile(clientName+compareFileName, fetchVersion(URL))
 		if err := fileScanner.Err(); err != nil {
 			log.Fatalf("Error while reading file %s", err)
 		}
 		if deepCompare(clientName+latestFileName, clientName+compareFileName) == false {
-			fmt.Println("Difference")
+			//fmt.Println("Difference")
 			removeFile(clientName + latestFileName)
 			renameFile(clientName+compareFileName, clientName+latestFileName)
 			appendHistory(clientName+versionHistoryFileName, URL)
 		} else {
-			fmt.Println("Same")
+			//fmt.Println("Same")
 			removeFile(clientName + compareFileName)
 		}
 	}
 	err = file.Close()
-	fmt.Printf("経過: %vms\n", time.Since(now).Milliseconds())
+	fmt.Printf("End: %vms\n", time.Since(now).Milliseconds())
 	if err != nil {
 		return
 	}
@@ -68,45 +65,31 @@ func fileExists(fileName string) bool {
 }
 
 func createFile(fileName, versionData string) {
+	now := time.Now()
 	err := ioutil.WriteFile(fileName, []byte(versionData), 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
+	fmt.Printf("createFile: %vms\n", time.Since(now).Milliseconds())
 }
 
 func fetchVersion(URL string) string {
+	now := time.Now()
 	curl := exec.Command("curl", URL)
+	fmt.Printf("fetchversion Curl: %vms\n", time.Since(now).Milliseconds())
+	// todo curl.Outputがすごいネック
 	out, err := curl.Output()
+	fmt.Printf("fetchversion Output: %vms\n", time.Since(now).Milliseconds())
 	if err != nil {
 		fmt.Println("error", err)
 		return "error"
 	}
+	fmt.Printf("fetchversion End: %vms\n", time.Since(now).Milliseconds())
 	return string(out)
 }
 
-//
-func writeLine(fileName, lines string) error {
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-
-		}
-	}(file)
-
-	for _, line := range lines {
-		_, err := file.WriteString(string(line))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func appendHistory(fileName, URL string) {
+	now := time.Now()
 	nowTime := time.Now().Format(dateLayout)
 	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
@@ -126,9 +109,11 @@ func appendHistory(fileName, URL string) {
 	if err != nil {
 		return
 	}
+	fmt.Printf("appendHistory: %vms\n", time.Since(now).Milliseconds())
 }
 
 func deepCompare(file1, file2 string) bool {
+	now := time.Now()
 	sf, err := os.Open(file1)
 	if err != nil {
 		log.Fatal(err)
@@ -146,17 +131,22 @@ func deepCompare(file1, file2 string) bool {
 			return false
 		}
 	}
+	fmt.Printf("compare: %vms\n", time.Since(now).Milliseconds())
 	return true
 }
 
 func removeFile(filename string) {
+	now := time.Now()
 	if err := os.Remove(filename); err != nil {
 		fmt.Println(err)
 	}
+	fmt.Printf("remove: %vms\n", time.Since(now).Milliseconds())
 }
 
 func renameFile(oldFilename, newFilename string) {
+	now := time.Now()
 	if err := os.Rename(oldFilename, newFilename); err != nil {
 		fmt.Println(err)
 	}
+	fmt.Printf("rename: %vms\n", time.Since(now).Milliseconds())
 }
